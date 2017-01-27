701fe327d3cb3b9543cd499eabe782c1a02294bb
godfpsn <godefroy.ponsinet@outlook.com>
2017-01-27 16:05:31 +0100

    Change Todo.cs file name to TodoListItem.cs

--- WebApi/Models/TodoListItem.cs
+++ WebApi/Models/TodoListItem.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.Models
 {
-    public class Todo
+    public class TodoListItem
     {
         public int Id { get; set; }
+        [Required]
         public string Description { get; set; }
+        public bool Done { get; set; } = false;
     }
 }
