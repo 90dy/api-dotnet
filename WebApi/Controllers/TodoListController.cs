9cfd90db1669a7a78818b7420c7f5ce13247c169
godfpsn <godefroy.ponsinet@outlook.com>
2017-01-27 17:55:13 +0100

    Add POST and PUT TodoListItem request

--- WebApi/Controllers/TodoListController.cs
+++ WebApi/Controllers/TodoListController.cs
@@ -6,26 +6,29 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Diagnostics;
 
 namespace WebApi.Controllers
 {
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class TodoListController : ApiController
     {
-        Todo[] todoList = new Todo[]
+        static IEnumerable<TodoListItem> todoList = new TodoListItem[]
         {
-            new Todo { Id = 1, Description = "I'm the first todo so don't delete me please." },
-            new Todo { Id = 2, Description = "I'm second, wtf ?!" },
-            new Todo { Id = 3, Description = "Third is a place, I don't give a sh** of the first !" },
-            new Todo { Id = 4, Description = "One day, fourth will have a cup too." }
+            new TodoListItem { Id = 1, Description = "I'm the first TodoListItem so don't delete me please.", Done = true },
+            new TodoListItem { Id = 2, Description = "I'm second, wtf ?!" },
+            new TodoListItem { Id = 3, Description = "Third is a place, I don't give a sh** of the first !" },
+            new TodoListItem { Id = 4, Description = "One day, fourth will have a cup too." }
         };
 
-        public IEnumerable<Todo> GetAllTodo()
+        [HttpGet]
+        public IEnumerable<TodoListItem> GetTodoList()
         {
             return todoList;
         }
 
-        public IHttpActionResult GetTodo(int id)
+        [HttpGet]
+        public IHttpActionResult GetTodoListItem(int id)
         {
             var todo = todoList.FirstOrDefault((t) => t.Id == id);
 
@@ -34,6 +37,41 @@ namespace WebApi.Controllers
                 return NotFound();
             }
             return Ok(todo);
+        }
+
+        [HttpPost]
+        public IHttpActionResult PostTodoListItem(TodoListItem item)
+        {
+            todoList = todoList.Concat(
+                new TodoListItem[]
+                {
+                    new TodoListItem
+                    {
+                        Id = todoList.Last().Id + 1,
+                        Description = item.Description,
+                        Done = item.Done
+                    }
+                }
+            );
+            return Ok();
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutTodoListItem(TodoListItem item)
+        {
+
+            TodoListItem todo = todoList.FirstOrDefault((t) => t.Id == item.Id);
+            Trace.WriteLine("Item: " + item.Id);
+
+            if (todo == null)
+                return (NotFound());
+            todoList = todoList.Select(t =>
+            {
+                if (t.Id == item.Id)
+                    return (item);
+                return (t);
+            });
+            return Ok();
         }
     }
 }
